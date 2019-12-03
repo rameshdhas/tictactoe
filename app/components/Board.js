9134b3c495d7578e5f79169fba0b3fae27d7696a
rameshdhason <drameshdhas@gmail.com>
2019-12-02 19:11:42 -0500

    first commit

--- app/components/Board.js
+++ app/components/Board.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View, TouchableOpacity, Button, Alert } from 'react-native';
 
-const instructions = Platform.select({
-    ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-    android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
-});
-
 export default class Board extends Component {
 
     constructor(props){
